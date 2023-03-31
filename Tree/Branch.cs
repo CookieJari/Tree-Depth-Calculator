e75e4cd892e936ce37077811984b40718eae5473
Jari <zalmanhajari@gmail.com>
2023-03-31 22:35:57 +0800

    finished making the tree

---
+++ Tree/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree
+{
+    class Branch
+    {
+        List<Branch> branches;
+
+        public void AddBranch(Branch branch)
+        {
+            branches.Add(branch);
+        }
+    }
+}
---
+++ Tree/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Branch root = new Branch();
+
+
+            Branch branch1 = new Branch();
+            Branch branch2 = new Branch();
+
+            Branch branch3 = new Branch();
+
+            Branch branch4 = new Branch();
+            Branch branch5 = new Branch();
+            Branch branch6 = new Branch();
+
+            Branch branch7 = new Branch();
+            Branch branch8 = new Branch();
+            Branch branch9 = new Branch();
+
+            Branch branch10 = new Branch();
+
+
+            root.AddBranch(branch1);
+            root.AddBranch(branch2);
+
+            branch1.AddBranch(branch3);
+
+            branch2.AddBranch(branch4);
+            branch2.AddBranch(branch5);
+            branch2.AddBranch(branch6);
+
+            branch4.AddBranch(branch7);
+
+            branch5.AddBranch(branch8);
+            branch5.AddBranch(branch9);
+
+            branch8.AddBranch(branch10);
+
+
+            
+        }
+    }
+}
