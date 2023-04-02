661f86c5768e0861232ed0537aefe34b7f0649f9
Jari <zalmanhajari@gmail.com>
2023-04-02 19:32:59 +0800

    Finished the recursive calulcation part

--- Tree/Branch.cs
+++ Tree/Branch.cs
@@ -8,11 +8,42 @@ namespace Tree
 {
     class Branch
     {
-        List<Branch> branches;
+        List<Branch> branches = new List<Branch>();
 
         public void AddBranch(Branch branch)
         {
             branches.Add(branch);
         }
+
+        // Doing a Depth First search
+        public int GetDepth(Branch branchInp)
+        {
+            // We first check if this node is the end
+            if (branchInp.branches.Count <= 0) 
+            {
+                return 1;
+            }
+            // If its not at the end we loop through all its children
+            else
+            {
+                // To keep track of the maximum depth
+                int maxDepth = 0;
+                // We loop through its children and call this function again so we can see its children's maximum depth first
+                for (int i = 0; i < branchInp.branches.Count; i++)
+                {
+                    int temp = branches[i].GetDepth(branches[i]);
+                    if (temp>=maxDepth)
+                    {
+                        maxDepth = temp;
+                    }
+                }
+                // After looping through its children we then add the depth of this node as well
+                maxDepth += 1;
+                return maxDepth;
+            }
+            
+        }
     }
+
+    
 }
--- Tree/Program.cs
+++ Tree/Program.cs
@@ -6,45 +6,38 @@ namespace Tree
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
+            // Creating all 10 nodes
             Branch root = new Branch();
-
-
             Branch branch1 = new Branch();
             Branch branch2 = new Branch();
-
             Branch branch3 = new Branch();
-
             Branch branch4 = new Branch();
             Branch branch5 = new Branch();
             Branch branch6 = new Branch();
-
             Branch branch7 = new Branch();
             Branch branch8 = new Branch();
             Branch branch9 = new Branch();
-
             Branch branch10 = new Branch();
 
-
+            // Connecting the nodes to each other
             root.AddBranch(branch1);
             root.AddBranch(branch2);
 
             branch1.AddBranch(branch3);
-
             branch2.AddBranch(branch4);
             branch2.AddBranch(branch5);
             branch2.AddBranch(branch6);
 
             branch4.AddBranch(branch7);
-
             branch5.AddBranch(branch8);
             branch5.AddBranch(branch9);
 
             branch8.AddBranch(branch10);
 
+            // Get the depth of the tree from this node (root node)
+            int ans = root.GetDepth(root);
 
-            
+            Console.WriteLine("FINAL DEPTH: " + ans);      
         }
     }
 }
